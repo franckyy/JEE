48d9e5dd014ba716069f35f2606eb02601be8e6f
franckyy <franck.calestroupat@gmail.com>
2015-07-15 14:09:40 +0200

    créatiojn du bean Client

---
+++ webfirstJDBC/src/com/loncoto/webfirstjdbc/beans/Client.java
@@ -0,0 +1,29 @@
+package com.loncoto.webfirstjdbc.beans;
+
+public class Client {
+	//ATTRIBUTS
+	private String nom;
+	private String email;
+	private double solde;
+	
+	//GETTERS & SETTERS
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public double getSolde() {
+		return solde;
+	}
+	public void setSolde(double solde) {
+		this.solde = solde;
+	}
+	
+}
--- webfirstJDBC/src/com/loncoto/webfirstjdbc/utils/DataBaseManager.java
+++ webfirstJDBC/src/com/loncoto/webfirstjdbc/utils/DataBaseManager.java
@@ -1,5 +1,9 @@
 package com.loncoto.webfirstjdbc.utils;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
@@ -9,6 +13,10 @@ import javax.servlet.ServletContextListener;
  */
 public class DataBaseManager implements ServletContextListener {
 
+	//déclaration d'un attribut INTERFACE. c'est le serveur qui se charge d'attribuer cette interface 
+	//à une classe en rapport avec la base de données.
+	private Connection base;
+	
     /**
      * Default constructor. 
      */
@@ -20,17 +28,43 @@ public class DataBaseManager implements ServletContextListener {
      * @see ServletContextListener#contextDestroyed(ServletContextEvent)
      */
     public void contextDestroyed(ServletContextEvent arg0)  { 
-         // TODO Auto-generated method stub
+         try {
+			base.close();
+			
+			System.out.println("connexion close");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 
 	/**
      * @see ServletContextListener#contextInitialized(ServletContextEvent)
      */
-    public void contextInitialized(ServletContextEvent arg0)  { 
+    public void contextInitialized(ServletContextEvent evt)  { 
          // méthode d'initialisation
     	//On peut lui demander de se connecter à la base et de la charger
     	System.out.println("démarrage de databasemanager");
     	
+    	String driverclass = evt.getServletContext().getInitParameter("driverclass");
+    	String databaseURL = evt.getServletContext().getInitParameter("databaseURL");
+    	String login = evt.getServletContext().getInitParameter("login");
+    	String password = evt.getServletContext().getInitParameter("password");
+    	
+    	try {
+    		//chargement du driver mysql
+    		Class.forName(driverclass);
+			
+    		//connexion à la base
+			base = DriverManager.getConnection(databaseURL, login, password);
+			
+			System.out.println("connecté à la base");
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
     }
 	
 }
