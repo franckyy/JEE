009a3fa228d7d3d82409320d01b5dac32f9f5562
franckyy <franck.calestroupat@gmail.com>
2015-07-15 11:37:04 +0200

    Création Listener DataBaseManager

---
+++ webfirstJDBC/src/com/loncoto/webfirstjdbc/utils/DataBaseManager.java
@@ -0,0 +1,33 @@
+package com.loncoto.webfirstjdbc.utils;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * Application Lifecycle Listener implementation class DataBaseManager
+ *
+ */
+public class DataBaseManager implements ServletContextListener {
+
+    /**
+     * Default constructor. 
+     */
+    public DataBaseManager() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent arg0)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent arg0)  { 
+         // TODO Auto-generated method stub
+    }
+	
+}
