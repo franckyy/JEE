65286a17b429c3967055a07915ef69037ac6bb75
franckyy <franck.calestroupat@gmail.com>
2015-07-16 09:24:43 +0200

    fabrication des objet dans DAO

--- chez_yen/src/com/chezyen/appli/utils/DAO.java
+++ chez_yen/src/com/chezyen/appli/utils/DAO.java
@@ -1,23 +1,36 @@
 package com.chezyen.appli.utils;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import com.chezyen.appli.beans.Client;
+import com.chezyen.appli.beans.Conditionnement;
+import com.chezyen.appli.beans.LigneCommande;
+import com.chezyen.appli.beans.Produit;
 
 public class DAO {
 
 	//DECLARATIONS
-	ArrayList<Client> arrayClients = null;
+	private ArrayList<Client> arrayClients = null;
+	private ArrayList<Produit> arrayProduits = null;
+	private ArrayList<Conditionnement> arrayCondi = null;
+	private ArrayList<LigneCommande> arrayLignes = null;
 	
 	//CONSTRUCTEUR
 	public DAO() {
 		super();
 		setClients();
+		setProduits();
+		setLignesCommande();
 	}
 	
-	//METHODES
-	private List<Client> setClients() {
+	//SETTERS
+	private void setLignesCommande() {
+		arrayLignes = new ArrayList<LigneCommande>();
+		
+		LigneCommande ligneA = new LigneCommande(1, 1, 10);
+	}
+	
+	private void setClients() {
 		arrayClients= new ArrayList<Client>();
 		
 		Client clientA = new Client(1, "Arnold", "Schwartzeneger", "New York");
@@ -29,7 +42,29 @@ public class DAO {
 		arrayClients.add(clientB);
 		arrayClients.add(clientC);
 		arrayClients.add(clientD);
+	}
+	
+	private void setProduits() {
+		arrayProduits =  new ArrayList<Produit>();
 		
+		Produit produitA = new Produit(1, "nem");
+		Produit produitB = new Produit(2, "samoussa");
+		Produit produitC = new Produit(3, "crevettes sauce piquante");
+		Produit produitD = new Produit(4, "poulet curry");
+		
+		arrayProduits.add(produitA);
+		arrayProduits.add(produitB);
+		arrayProduits.add(produitC);
+		arrayProduits.add(produitD);
+	}
+	
+	//GETTERS
+	public ArrayList<Client> getClients() {
 		return arrayClients;
+	}
+	
+
+	public ArrayList<Produit> getProduits() {
+		return arrayProduits;
 	}
 }
