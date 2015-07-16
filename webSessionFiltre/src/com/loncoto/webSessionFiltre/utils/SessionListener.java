fcec4c8dbe82733edeaffe558e4d4e15de6389cb
franckyy <franck.calestroupat@gmail.com>
2015-07-16 14:34:51 +0200

    création listener

---
+++ webSessionFiltre/src/com/loncoto/webSessionFiltre/utils/SessionListener.java
@@ -0,0 +1,55 @@
+package com.loncoto.webSessionFiltre.utils;
+
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionBindingEvent;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+/**
+ * Application Lifecycle Listener implementation class SessionListener
+ *
+ */
+public class SessionListener implements HttpSessionListener, HttpSessionAttributeListener {
+
+    /**
+     * Default constructor. 
+     */
+    public SessionListener() {
+    }
+
+	/**
+     * @see HttpSessionListener#sessionCreated(HttpSessionEvent)
+     */
+    public void sessionCreated(HttpSessionEvent arg0)  { 
+    	System.out.println("session créée : " + arg0.getSession().getId());
+    }
+
+	/**
+     * @see HttpSessionListener#sessionDestroyed(HttpSessionEvent)
+     */
+    public void sessionDestroyed(HttpSessionEvent arg0)  { 
+    	System.out.println("session détruite : " + arg0.getSession().getId());
+    }
+
+	/**
+     * @see HttpSessionAttributeListener#attributeAdded(HttpSessionBindingEvent)
+     */
+    public void attributeAdded(HttpSessionBindingEvent arg0)  { 
+    	System.out.println("attribut ajouté : " + arg0.getName() + " -> " + arg0.getValue());
+    }
+
+	/**
+     * @see HttpSessionAttributeListener#attributeRemoved(HttpSessionBindingEvent)
+     */
+    public void attributeRemoved(HttpSessionBindingEvent arg0)  {
+    	System.out.println("attribut enlevé : " + arg0.getName() + " -> " + arg0.getValue());
+    }
+
+	/**
+     * @see HttpSessionAttributeListener#attributeReplaced(HttpSessionBindingEvent)
+     */
+    public void attributeReplaced(HttpSessionBindingEvent arg0)  { 
+    	System.out.println("attribut remplacé : " + arg0.getName() + " -> " + arg0.getValue());
+    }
+	
+}
